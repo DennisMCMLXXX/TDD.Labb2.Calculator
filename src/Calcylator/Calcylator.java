e4c753674d4f881805ed887c7f8645a798357bbc
Dennis Larsson <tryckare@gmail.com>
2019-03-03 22:18:22 +0100

    Working on addition

--- src/Calcylator/Calcylator.java
+++ src/Calcylator/Calcylator.java
@@ -2,6 +2,20 @@ package Calcylator;
 
 public class Calcylator {
     public double calculateExpression(String input) {
-        return Double.parseDouble(input);
+
+        if(input.contains("+")) {
+            return addition(input);
+        }
+
+        if(input.matches("[0-9]+")) {
+            return Double.parseDouble(input);
+        }
+
+    return Double.NaN;
+    }
+
+    private double addition(String input){
+        String[] components = input.split("[+]");
+        return calculateExpression(components[0]) + calculateExpression(components[1]);
     }
 }
--- src/CalcylatorTest/CalcylatorTest.java
+++ src/CalcylatorTest/CalcylatorTest.java
@@ -8,36 +8,42 @@ import static org.junit.Assert.assertEquals;
 
 public class CalcylatorTest{
 
+    private static final double DELTA = 0.111;
+
+    Calcylator calc;
 
     @Before
-    public void init() {
-
+    public void beforeEachTest() {
+        calc = new Calcylator();
     }
 
     @Ignore
     @Test
     public void featureTestAdditinMultiplySubtractionInRinghtOrder(){
-        Calcylator calc = new Calcylator();
-        String input = "3+4*5-6";
+         String input = "3+4*5-6";
         double acual = calc.calculateExpression(input);
-        assertEquals(17d, acual, 0.111 );
+        assertEquals(17d, acual, DELTA);
     }
 
     @Test
     public void StringOfOneToDoubleOne(){
-        Calcylator calc = new Calcylator();
         String input = "1";
         double actual = calc.calculateExpression(input);
-        assertEquals(1d, actual, 0.111);
+        assertEquals(1d, actual, DELTA);
     }
 
     @Test
     public void StringOfTwoGivesDoubleTwo(){
-        Calcylator calc = new Calcylator();
         String input = "2";
         double actual = calc.calculateExpression(input);
-        assertEquals(2d, actual, 0.111);
+        assertEquals(2d, actual, DELTA);
     }
 
+    @Test
+    public void AdditionOnePlusOneEqualsTwo(){
+        String input = "1+1";
+        double acutal = calc.calculateExpression(input);
+        assertEquals(2d, acutal, DELTA);
+    }
 
 }
